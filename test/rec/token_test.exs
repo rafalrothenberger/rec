99b634440ac606ec3d7fef06e83eb3d0f269a471
Rafał Rothenberger <rafal.rothenebrger@gmail.com>
2019-02-23 00:09:22 +0100

    Added test for token validation

--- test/rec/token_test.exs
+++ test/rec/token_test.exs
@@ -1,9 +1,17 @@
 defmodule Rec.TokenTest do
   use ExUnit.Case, async: true
 
-  test "jwt token generation for user" do
-    {:ok, jwt, _} = Rec.Token.sign(1)
-    assert "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.3vkBf8j6l5onmw1QZi4pG9-VOg6UEPQxpRElUKP3FF4" = jwt
+  describe "jwt token" do
+
+    test "jwt token generation for user" do
+      {:ok, jwt, _} = Rec.Token.sign(1)
+      assert "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.3vkBf8j6l5onmw1QZi4pG9-VOg6UEPQxpRElUKP3FF4" = jwt
+    end
+
+    test "jwt token validation" do
+      {:ok, 1} = Rec.Token.valid?("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.3vkBf8j6l5onmw1QZi4pG9-VOg6UEPQxpRElUKP3FF4")
+    end
+
   end
 
 end
