a0646c510c63f619619962aaaa0209cc4d6d0e6e
Rafał Rothenberger <rafal.rothenebrger@gmail.com>
2019-02-22 23:00:29 +0100

    Fixed token test, was assigning string to jwt so assert was always passing

--- test/rec/token_test.exs
+++ test/rec/token_test.exs
@@ -3,7 +3,7 @@ defmodule Rec.TokenTest do
 
   test "jwt token generation for user" do
     {:ok, jwt, _} = Rec.Token.sign(1)
-    assert jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.3vkBf8j6l5onmw1QZi4pG9-VOg6UEPQxpRElUKP3FF4"
+    assert "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.3vkBf8j6l5onmw1QZi4pG9-VOg6UEPQxpRElUKP3FF4" = jwt
   end
 
 end
