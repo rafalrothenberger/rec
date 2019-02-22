af762e388b19eec48f1523cdf68febf574bdd17b
Rafał Rothenberger <rafal.rothenebrger@gmail.com>
2019-02-23 00:11:04 +0100

    Added validation for user tokens in Rec.Token

--- lib/rec/token.ex
+++ lib/rec/token.ex
@@ -10,4 +10,11 @@ defmodule Rec.Token do
     generate_and_sign(%{"id" => id})
   end
 
+  def valid?(token) do
+    case verify_and_validate(token)  do
+      {:ok, %{"id" => id}} -> {:ok, id}
+      {:error, _} -> :error
+    end
+  end
+
 end
